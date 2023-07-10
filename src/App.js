c358976c57427d3ceb15caa209e8b9c9c0017d2d
Deyvsonf17 <deyvsonf9@gmail.com>
2023-07-10 11:45:46 -0300

    pp2

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Segment } from 'semantic-ui-react';
+import './App.css';
+import Home from './views/home/Home';
+
+function App() {
+
+  return (
+
+    <div className="App">
+      
+      <Home />
+
+      <div style={{marginTop: '6%'}}>
+        <Segment vertical color='grey' size='tiny' textAlign='center'>
+          &copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes
+        </Segment>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
